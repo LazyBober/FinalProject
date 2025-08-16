02a82646271edc69e5dd82f72e78c8fc065c38c7
Fonty-Lex <kicenaleksej@gmail.com>
2025-08-16 23:32:16 +0300

    зробив все що написав у тг

---
+++ Project/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OpenInventory()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
