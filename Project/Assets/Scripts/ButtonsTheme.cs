786674eae8d78d056eb2b4f3442c169716342401
LazyBober <van.shcherbakov@gmail.com>
2025-08-28 16:56:40 +0300

    почав робити налаштування(темну на світлу тему)

---
+++ Project/Assets/Scripts/ButtonsTheme.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsTheme : MonoBehaviour
+{
+    ThemeData themeData;
+
+    List<Button> buttons;  
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+
+    void Start()
+    {
+        if (themeData.Theme == 0)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                buttons[i].
+            }
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Project/Assets/Scripts/ThemeData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ThemeSwitch", menuName = "ThemeSwitch")]
+public class ThemeData : ScriptableObject
+{
+    [SerializeField] int theme;
+
+    public int Theme
+    {
+        get => theme;
+        set => theme = value;
+    }
+}
