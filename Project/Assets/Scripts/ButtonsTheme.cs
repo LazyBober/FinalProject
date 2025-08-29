f95ea7f331e978e189ff75ed456d5685335e304f
LazyBober <van.shcherbakov@gmail.com>
2025-08-29 19:38:45 +0300

    доробив світлу/темну тему але потім треба буде вдосконалити

--- Project/Assets/Scripts/ButtonsTheme.cs
+++ Project/Assets/Scripts/ButtonsTheme.cs
@@ -5,31 +5,64 @@ using UnityEngine.UI;
 
 public class ButtonsTheme : MonoBehaviour
 {
-    ThemeData themeData;
+    public ThemeData themeData;
 
-    List<Button> buttons;  
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
-    Button button5;
-    Button button6;
-    Button button7;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
 
+    List<Button> buttons = new List<Button>();
+    
     void Start()
     {
-        if (themeData.Theme == 0)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                buttons[i].
-            }
-        }
+        buttons.Add(button1); 
+        buttons.Add(button2); 
+        buttons.Add(button3); 
+        buttons.Add(button4); 
+        buttons.Add(button5); 
+        buttons.Add(button6); 
+        buttons.Add(button7);
     }
 
     
     void Update()
     {
-        
+        if (themeData.Theme == 0)
+        {
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                if (buttons[i])
+                {
+                    buttons[i].image.color = Color.black;
+                }
+            }
+        }
+        if (themeData.Theme == 1)
+        {
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                if (buttons[i])
+                {
+                    buttons[i].image.color = Color.white;
+                }
+            }
+        }
+    }
+
+    public void SwithThemes()
+    {
+        switch (themeData.Theme)
+        {
+            case 0:
+                themeData.Theme = 1;
+                break;
+            case 1:
+                themeData.Theme = 0;
+                break;
+        }
     }
 }
