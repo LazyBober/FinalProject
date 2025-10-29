d2cb60acbfbca223afe83d241223d294db445eb7
Fonty-Lex <kicenaleksej@gmail.com>
2025-10-29 14:32:28 +0200

    зробив переміщення кнопок
    переміщення 3 кнопок на іншу сторону екрану при натисканні одної з них

---
+++ Project/Assets/Scripts/MovingTheButtonScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveAllButtons : MonoBehaviour
+{
+    [SerializeField] private Button[] buttons; 
+    private bool moveRight = true;            
+
+    void Start()
+    {
+        foreach (Button btn in buttons)
+        {
+            btn.onClick.AddListener(MoveButtons);
+        }
+    }
+
+    void MoveButtons()
+    {
+        foreach (Button btn in buttons)
+        {
+            RectTransform rect = btn.GetComponent<RectTransform>();
+            Vector2 pos = rect.anchoredPosition;
+
+            float direction = moveRight ? 1f : -1f;
+
+            pos.x += 300f * direction;
+
+            rect.anchoredPosition = pos;
+        }
+
+        moveRight = !moveRight;
+    }
+}
