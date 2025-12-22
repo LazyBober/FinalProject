080219ded7455f12dd0972ac473a7411b9e220b4
Fonty-Lex <kicenaleksej@gmail.com>
2025-12-22 15:57:38 +0200

    не встигаю
    I made it so that the cart moves only from the square, but firstly, the red cart moves from both the red and blue squares, and secondly, they move even if the square hasn't been set yet. Now they'll turn off the lights; I'll deal with these problems later.

---
+++ Project/Assets/Scripts/BlueCartScript.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class CartScript : MonoBehaviour
+{
+    [Header("Prefab that can push this cart")]
+    [SerializeField] private GameObject allowedShapePrefab;
+
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private bool moveRight = true;
+
+    private bool canMove = false;
+
+    private string allowedPrefabName;
+
+    private void Awake()
+    {
+        // сохраняем имя префаба
+        if (allowedShapePrefab != null)
+            allowedPrefabName = allowedShapePrefab.name;
+    }
+
+    private void Update()
+    {
+        if (!canMove) return;
+
+        float dir = moveRight ? -1f : 1f;
+        transform.position += new Vector3(dir * moveSpeed * Time.deltaTime, 0f, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (allowedShapePrefab == null) return;
+
+        // Проверка: объект создан из нужного префаба
+        if (collision.gameObject.name == allowedPrefabName + "(Clone)")
+        {
+            canMove = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (allowedShapePrefab == null) return;
+
+        if (collision.gameObject.name == allowedPrefabName + "(Clone)")
+        {
+            canMove = false;
+        }
+    }
+}
+
--- Project/Assets/Scripts/ThemeData.cs
+++ Project/Assets/Scripts/ThemeData.cs
@@ -1,3 +1,8 @@
+
+
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New ThemeSwitch", menuName = "ThemeSwitch")]
